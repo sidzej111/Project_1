f66e0ed13c4c3e4437768cbbc104872079eccba3
sidzej111 <sidzej111@gmail.com>
2024-11-25 20:13:49 +0100

    Nový soubor

--- skript1.py
+++ skript1.py
@@ -2,3 +2,8 @@ print("Ahoj, těším se na další lekce v Pythonu")
 print("Těší mě")
 print("provedl jsem změny")
 print("projekt1")
+print("další pokus")
+print("další pokus")
+print("změna")
+print("změna")
+

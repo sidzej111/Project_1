7bd1d8e214644441e193c9c09f30a25672a79b4b
sidzej111 <sidzej111@gmail.com>
2024-12-11 21:27:57 +0100

    Cviceni

---
+++ cviceni4 lekce1.py
@@ -0,0 +1,11 @@
+# cviceni 4 - vytvorit program
+
+jmeno = "Lukáš"
+prijmeni = "Dvořák"
+cele_jmeno = jmeno + " " + prijmeni
+delka_jmena = len(cele_jmeno)
+print("Celé jméno:", cele_jmeno)
+print("Délka jmena:", delka_jmena)
+print("=" * delka_jmena)
+print(cele_jmeno)
+print("=" * delka_jmena)

469d00ced705c3282dfa82cb585bdad6025ae188
sidzej111 <sidzej111@gmail.com>
2025-01-27 13:04:58 +0100

    zmena

--- skript1.py
+++
@@ -1,9 +0,0 @@
-print("Ahoj, těším se na další lekce v Pythonu")
-print("Těší mě")
-print("provedl jsem změny")
-print("projekt1")
-print("další pokus")
-print("další pokus")
-print("změna")
-print("změna")
-
